08f26f025656013d6229b4aa0fe27f0179dbabc8
Michael W <baddaemon87@gmail.com>
2023-07-16 17:27:50 +0200

    Dialer: Remove unused Fragments [3/x]
    ... and the now unused other code
    
    Change-Id: I5b0d82e487dc20347bfe6b97f3dd5df4579cac1d

--- java/com/android/dialer/binary/common/DialerApplication.java
+++ java/com/android/dialer/binary/common/DialerApplication.java
@@ -19,10 +19,6 @@ package com.android.dialer.binary.common;
 import android.app.Application;
 import android.os.Trace;
 import android.support.annotation.NonNull;
-import com.android.dialer.calllog.CallLogComponent;
-import com.android.dialer.calllog.CallLogFramework;
-import com.android.dialer.calllog.config.CallLogConfig;
-import com.android.dialer.calllog.config.CallLogConfigComponent;
 import com.android.dialer.common.LogUtil;
 import com.android.dialer.common.concurrent.DialerExecutorComponent;
 import com.android.dialer.inject.HasRootComponent;
@@ -40,22 +36,9 @@ public abstract class DialerApplication extends Application implements HasRootCo
     Trace.beginSection("DialerApplication.onCreate");
     StrictModeComponent.get(this).getDialerStrictMode().onApplicationCreate(this);
     super.onCreate();
-    initializeAnnotatedCallLog();
     PersistentLogger.initialize(this);
     NotificationChannelManager.initChannels(this);
     Trace.endSection();
-  }
-
-  private void initializeAnnotatedCallLog() {
-    CallLogConfig callLogConfig = CallLogConfigComponent.get(this).callLogConfig();
-    callLogConfig.schedulePollingJob();
-
-    if (callLogConfig.isCallLogFrameworkEnabled()) {
-      CallLogFramework callLogFramework = CallLogComponent.get(this).callLogFramework();
-      callLogFramework.registerContentObservers();
-    } else {
-      LogUtil.i("DialerApplication.initializeAnnotatedCallLog", "framework not enabled");
-    }
   }
 
   /**
