19e99f5fb9ce30cd546d39428c8776c2c3897879
Michael W <baddaemon87@gmail.com>
2023-07-19 22:40:46 +0200

    Dialer: more possible cleanups [6/x]
    Change-Id: Idc94a7f4f21230d12c76879a6a7e4e54f924d991

--- java/com/android/dialer/app/settings/DialerSettingsActivity.java
+++ java/com/android/dialer/app/settings/DialerSettingsActivity.java
@@ -30,7 +30,6 @@ import android.telecom.TelecomManager;
 import android.telephony.TelephonyManager;
 import android.view.MenuItem;
 import android.widget.Toast;
-import com.android.dialer.about.AboutPhoneFragment;
 import com.android.dialer.app.R;
 import com.android.dialer.blocking.FilteredNumbersUtil;
 import com.android.dialer.common.LogUtil;
@@ -149,13 +148,6 @@ public class DialerSettingsActivity extends AppCompatPreferenceActivity {
       accessibilitySettingsHeader.intent = accessibilitySettingsIntent;
       target.add(accessibilitySettingsHeader);
     }
-
-    if (showAbout()) {
-      Header aboutPhoneHeader = new Header();
-      aboutPhoneHeader.titleRes = R.string.about_phone_label;
-      aboutPhoneHeader.fragment = AboutPhoneFragment.class.getName();
-      target.add(aboutPhoneHeader);
-    }
   }
 
   private void addVoicemailSettings(List<Header> target, boolean isPrimaryUser) {
@@ -224,11 +216,6 @@ public class DialerSettingsActivity extends AppCompatPreferenceActivity {
       }
     }
     return result;
-  }
-
-  /** Whether "about" should be shown in settings. Override to hide about. */
-  public boolean showAbout() {
-    return true;
   }
 
   /**
