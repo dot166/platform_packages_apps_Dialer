2800b5f6c966ec00c41c5453d708c9ee90bfe1f7
Michael W <baddaemon87@gmail.com>
2023-07-19 22:40:46 +0200

    Dialer: more possible cleanups [6/x]
    Change-Id: Idc94a7f4f21230d12c76879a6a7e4e54f924d991

--- java/com/android/incallui/StatusBarNotifier.java
+++ java/com/android/incallui/StatusBarNotifier.java
@@ -433,7 +433,6 @@ public class StatusBarNotifier
     TelecomAdapter.getInstance().startForegroundNotification(NOTIFICATION_ID, notification);
 
     Trace.endSection();
-    call.getLatencyReport().onNotificationShown();
     currentNotification = notificationType;
     Trace.endSection();
   }
