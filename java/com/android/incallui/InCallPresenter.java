5af238f52f3389be3e758c7b25afe6f07ea57700
Michael W <baddaemon87@gmail.com>
2023-07-29 14:07:34 +0200

    Dialer: Remove NeededForTesting and VisibleForTesting [9/x]
    * We don't have tests, so we don't need these annotations
    * Get rid of the code that was just for tests
    
    Change-Id: I367812cdfbc5392b6da2e3bb0ab136d99450fd0f

--- java/com/android/incallui/InCallPresenter.java
+++ java/com/android/incallui/InCallPresenter.java
@@ -25,7 +25,6 @@ import android.provider.BlockedNumberContract;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
 import android.telecom.Call.Details;
 import android.telecom.CallAudioState;
 import android.telecom.DisconnectCause;
@@ -258,7 +257,6 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
   private boolean automaticallyMutedByAddCall = false;
 
   /** Inaccessible constructor. Must use getRunningInstance() to get this singleton. */
-  @VisibleForTesting
   InCallPresenter() {}
 
   public static synchronized InCallPresenter getInstance() {
@@ -268,11 +266,6 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
       Trace.endSection();
     }
     return inCallPresenter;
-  }
-
-  @VisibleForTesting
-  public static synchronized void setInstanceForTesting(InCallPresenter inCallPresenter) {
-    InCallPresenter.inCallPresenter = inCallPresenter;
   }
 
   /**
@@ -1613,11 +1606,6 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
 
   public ThemeColorManager getThemeColorManager() {
     return themeColorManager;
-  }
-
-  @VisibleForTesting
-  public void setThemeColorManager(ThemeColorManager themeColorManager) {
-    this.themeColorManager = themeColorManager;
   }
 
   /** Called when the foreground call changes. */
