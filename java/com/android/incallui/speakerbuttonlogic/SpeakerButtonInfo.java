cfe609d9a3aaf81b5cb3e2e411f61d52494b69f7
Michael W <baddaemon87@gmail.com>
2023-08-04 22:39:45 +0200

    Dialer: InCallUI: Show chosen audio text
    * Right now it's only ever "Sound"
    * When connected to a bluetooth device, show it's alias, otherwise
      show headset, telephone or speakers, depending on what's in use
    
    Change-Id: I38a9e748b56947dbe5202396ca290d981bd040d6

--- java/com/android/incallui/speakerbuttonlogic/SpeakerButtonInfo.java
+++ java/com/android/incallui/speakerbuttonlogic/SpeakerButtonInfo.java
@@ -20,6 +20,10 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.IntDef;
 import android.support.annotation.StringRes;
 import android.telecom.CallAudioState;
+
+import com.android.dialer.R;
+import com.android.incallui.util.BluetoothUtil;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -41,32 +45,37 @@ public class SpeakerButtonInfo {
   @StringRes public final int label;
   public final boolean nonBluetoothMode;
   public final boolean isChecked;
+  public String deviceName;
 
   public SpeakerButtonInfo(CallAudioState audioState) {
     if ((audioState.getSupportedRouteMask() & CallAudioState.ROUTE_BLUETOOTH)
         == CallAudioState.ROUTE_BLUETOOTH) {
       nonBluetoothMode = false;
-      label = R.string.incall_label_audio;
 
       if ((audioState.getRoute() & CallAudioState.ROUTE_BLUETOOTH)
           == CallAudioState.ROUTE_BLUETOOTH) {
         icon = R.drawable.volume_bluetooth;
         contentDescription = R.string.incall_content_description_bluetooth;
         isChecked = true;
+        label = R.string.audioroute_bluetooth;
+        deviceName = BluetoothUtil.getAliasName(audioState.getActiveBluetoothDevice());
       } else if ((audioState.getRoute() & CallAudioState.ROUTE_SPEAKER)
           == CallAudioState.ROUTE_SPEAKER) {
         icon = R.drawable.quantum_ic_volume_up_vd_theme_24;
         contentDescription = R.string.incall_content_description_speaker;
         isChecked = true;
+        label = R.string.audioroute_speaker;
       } else if ((audioState.getRoute() & CallAudioState.ROUTE_WIRED_HEADSET)
           == CallAudioState.ROUTE_WIRED_HEADSET) {
         icon = R.drawable.quantum_ic_headset_vd_theme_24;
         contentDescription = R.string.incall_content_description_headset;
         isChecked = true;
+        label = R.string.audioroute_headset;
       } else {
         icon = R.drawable.quantum_ic_phone_in_talk_vd_theme_24;
         contentDescription = R.string.incall_content_description_earpiece;
         isChecked = false;
+        label = R.string.audioroute_phone;
       }
     } else {
       nonBluetoothMode = true;
