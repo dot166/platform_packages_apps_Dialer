38eebb9f01722c28779c39a119eb42399d7c99fe
Michael W <baddaemon87@gmail.com>
2023-08-05 21:51:18 +0200

    Dialer: Remove EnrichedCall [16/x]
    It's not hooked up besides simulation (we have a stub, though!)
    
    Change-Id: Ia56d30a717dc429872d2def9dab5289b4841269a

--- java/com/android/incallui/CallButtonPresenter.java
+++ java/com/android/incallui/CallButtonPresenter.java
@@ -620,9 +620,6 @@ public class CallButtonPresenter
   public void onInternationalCallOnWifi() {}
 
   @Override
-  public void onEnrichedCallSessionUpdate() {}
-
-  @Override
   public Context getContext() {
     return context;
   }
