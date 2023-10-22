1c0f48f0382a260b1bd270c006159260e5b4aa82
Michael W <baddaemon87@gmail.com>
2023-10-22 14:42:48 +0200

    Dialer: Fix settings

--- java/com/android/dialer/app/settings/DialerSettingsActivity.java
+++ java/com/android/dialer/app/settings/DialerSettingsActivity.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
- * Copyright (C) 2023 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +21,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserManager;
+import android.preference.PreferenceManager;
 import android.provider.BlockedNumberContract;
 import android.provider.Settings;
 import android.telecom.PhoneAccount;
@@ -32,7 +32,6 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.preference.PreferenceManager;
 
 import com.android.dialer.app.R;
 import com.android.dialer.common.LogUtil;
@@ -41,7 +40,6 @@ import com.android.dialer.proguard.UsedByReflection;
 import com.android.dialer.util.PermissionsUtil;
 import com.android.dialer.voicemail.settings.VoicemailSettingsFragment;
 import com.android.voicemail.VoicemailClient;
-
 import java.util.List;
 
 /** Activity for dialer settings. */
