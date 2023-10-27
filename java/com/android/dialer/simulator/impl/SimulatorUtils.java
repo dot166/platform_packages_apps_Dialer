e27cbeb870d7282fdaa8dd98e25b9c9031168be6
Michael W <baddaemon87@gmail.com>
2023-10-27 15:52:16 +0200

    Dialer: Remove persistent logger
    * We don't use it
    * Simulator: crashed on sharing anyway
    * Generally not worth keeping
    
    Change-Id: I5bd966b84997405dc884e7c6b800002770bbe1b7

--- java/com/android/dialer/simulator/impl/SimulatorUtils.java
+++ java/com/android/dialer/simulator/impl/SimulatorUtils.java
@@ -32,7 +32,6 @@ import com.android.dialer.databasepopulator.BlockedBumberPopulator;
 import com.android.dialer.databasepopulator.CallLogPopulator;
 import com.android.dialer.databasepopulator.ContactsPopulator;
 import com.android.dialer.databasepopulator.VoicemailPopulator;
-import com.android.dialer.persistentlog.PersistentLogger;
 import com.android.dialer.preferredsim.PreferredSimFallbackContract;
 import java.util.ArrayList;
 import java.util.List;
@@ -105,23 +104,6 @@ public class SimulatorUtils {
     context.sendBroadcast(intent);
   }
 
-  public static void sharePersistentLog(@NonNull Context context) {
-    DialerExecutorComponent.get(context)
-        .dialerExecutorFactory()
-        .createNonUiTaskBuilder(new ShareLogWorker())
-        .onSuccess(
-            (String log) -> {
-              Intent intent = new Intent(Intent.ACTION_SEND);
-              intent.setType("text/plain");
-              intent.putExtra(Intent.EXTRA_TEXT, log);
-              if (intent.resolveActivity(context.getPackageManager()) != null) {
-                context.startActivity(intent);
-              }
-            })
-        .build()
-        .executeSerial(null);
-  }
-
   public static void addVoicemailNotifications(@NonNull Context context, int notificationNum) {
     LogUtil.enterBlock("SimulatorNotifications.addVoicemailNotifications");
     List<ContentValues> voicemails = new ArrayList<>();
@@ -183,14 +165,6 @@ public class SimulatorUtils {
     public Void doInBackground(Context context) {
       context.getContentResolver().delete(PreferredSimFallbackContract.CONTENT_URI, null, null);
       return null;
-    }
-  }
-
-  private static class ShareLogWorker implements Worker<Void, String> {
-    @Nullable
-    @Override
-    public String doInBackground(Void unused) {
-      return PersistentLogger.dumpLogToString();
     }
   }
 
