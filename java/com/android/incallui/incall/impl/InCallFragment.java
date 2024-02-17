8e5d4ac50e9130d7f890c5f7375626588986c0d5
Michael W <baddaemon87@gmail.com>
2024-02-17 16:25:29 +0100

    Dialer: Request BLUETOOTH_CONNECT
    * When we want to use the bluetooth device alias, we require the
      permission first.
    * Ask the user to grant the permission but don't enforce it - we have
      a default string being shown
    
    Change-Id: Ifa3733595741c1481ae0ab69f5026bb4f05f2a9a

--- java/com/android/incallui/incall/impl/InCallFragment.java
+++ java/com/android/incallui/incall/impl/InCallFragment.java
@@ -16,6 +16,7 @@
 
 package com.android.incallui.incall.impl;
 
+import android.Manifest;
 import android.Manifest.permission;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -95,8 +96,20 @@ public class InCallFragment extends Fragment
   private int voiceNetworkType;
   private int phoneType;
   private boolean stateRestored;
+  private boolean userDeniedBluetooth;
 
   private static final int REQUEST_CODE_CALL_RECORD_PERMISSION = 1000;
+
+  private final ActivityResultLauncher<String[]> bluetoothPermissionLauncher =
+          registerForActivityResult(
+                  new ActivityResultContracts.RequestMultiplePermissions(),
+                  grantResults -> {
+                    boolean allGranted = grantResults.values().stream().allMatch(x -> x);
+                    inCallButtonUiDelegate.showAudioRouteSelector();
+                    if (!allGranted) {
+                      userDeniedBluetooth = true;
+                    }
+                  });
 
   // Add animation to educate users. If a call has enriched calling attachments then we'll
   // initially show the attachment page. After a delay seconds we'll animate to the button grid.
@@ -523,8 +536,22 @@ public class InCallFragment extends Fragment
 
   @Override
   public void showAudioRouteSelector() {
-    AudioRouteSelectorDialogFragment.newInstance(inCallButtonUiDelegate.getCurrentAudioState())
-        .show(getChildFragmentManager(), null);
+    String[] permissions = new String[]{Manifest.permission.BLUETOOTH_CONNECT};
+    if (hasAllPermissions(permissions) || userDeniedBluetooth) {
+      AudioRouteSelectorDialogFragment.newInstance(inCallButtonUiDelegate.getCurrentAudioState())
+              .show(getChildFragmentManager(), null);
+    } else {
+      bluetoothPermissionLauncher.launch(permissions);
+    }
+  }
+
+  private boolean hasAllPermissions(String[] permissions) {
+    for (String p : permissions) {
+      if (requireContext().checkSelfPermission(p) != PackageManager.PERMISSION_GRANTED) {
+        return false;
+      }
+    }
+    return true;
   }
 
   @Override
