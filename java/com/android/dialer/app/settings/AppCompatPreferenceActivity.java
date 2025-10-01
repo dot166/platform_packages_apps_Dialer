92cb7f33ce59b20bc51489a1ca41cf042f057015
._______166 <retromac9x2@gmail.com>
2025-10-01 14:21:29 +0100

    Dialer: Migrate to AndroidX
    Change-Id: I3776eac06f301ea61ce6adb5f0678937a4cc040d
    
    squashed with:
    
    create mapper script
    
    for required AndroidX conversion

--- java/com/android/dialer/app/settings/AppCompatPreferenceActivity.java
+++ java/com/android/dialer/app/settings/AppCompatPreferenceActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2023 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +19,12 @@ package com.android.dialer.app.settings;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 
 /**
  * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
